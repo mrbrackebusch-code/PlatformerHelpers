575a85fb807d1adf857fbee1eed34fd740b12d6c
mrbrackebusch-code <mrbrackebusch@gmail.com>
2026-09-15 06:40:36 -0700

    Add extension compile fixture

---
+++ test.ts
@@ -0,0 +1,31 @@
+let movingPlatform = platformerHelpers.createMovingPlatform(
+    image.create(16, 4),
+    2,
+    8,
+    8,
+    8,
+    30
+)
+
+let walkingEnemy = platformerHelpers.createWalkingEnemy(
+    image.create(16, 16),
+    10,
+    8,
+    25
+)
+
+let movingHazard = platformerHelpers.createMovingHazard(
+    image.create(16, 16),
+    12,
+    8,
+    12,
+    4,
+    35
+)
+
+platformerHelpers.onPlayerStompsEnemy(function (player, enemy) {
+    enemy.destroy()
+    player.vy = -100
+})
+
+
